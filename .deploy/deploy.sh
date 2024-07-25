076f43f38d5457012e8e2ba31181fbd37a9fc9f9
pixelea-fr <113612256+pixelea-fr@users.noreply.github.com>
2024-07-25 10:33:46 +0200

    Update deploy.sh

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "Starting SCSS compilation..."
+
+# Check if sass is installed
+if ! command -v sass &> /dev/null; then
+    echo "sass command not found. Exiting."
+    exit 1
+fi
+
 # Function to compile SCSS to CSS
 compile_scss() {
     local scss_file="$1"
@@ -11,8 +19,14 @@ compile_scss() {
     # Ensure the output directory exists
     mkdir -p "$(dirname "$css_file")"
 
+    echo "Compiling $scss_file to $css_file"
+
     # Compile SCSS to CSS using the globally installed sass
     sass "$scss_file":"$css_file" --style expanded
+    if [ $? -ne 0 ]; then
+        echo "Error compiling $scss_file"
+        exit 1
+    fi
 }
 
 # Find all SCSS files that do not start with an underscore and compile them
