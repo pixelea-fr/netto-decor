179ae48cd9a5a48f13010382566873c22169a3bc
Nicolas <nicolas.gehin@pixelea.fr>
2024-07-26 11:28:02 +0200

    anulation des modif pour swiper

--- functions/id-blog.php
+++ functions/id-blog.php
@@ -7,16 +7,3 @@ function ajouter_classe_blog_id($classes) {
     return $classes;
 }
 add_filter('body_class', 'ajouter_classe_blog_id');
-
-function desactiver_jetpack_block_slideshow() {
-    if ( ! is_admin() && is_multisite()) {
-        global $blog_id;
-        if ( $blog_id == 1 ){
-            wp_dequeue_script( 'jetpack-block-slideshow' );
-            wp_deregister_script( 'jetpack-block-slideshow' );
-            wp_dequeue_style( 'jetpack-block-slideshow');
-            wp_deregister_style( 'jetpack-block-slideshow');
-        }
-    }
-}
-add_action( 'wp_enqueue_scripts', 'desactiver_jetpack_block_slideshow', 100 );
